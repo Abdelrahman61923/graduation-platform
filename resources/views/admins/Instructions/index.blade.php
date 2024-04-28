d24d6cbb3c9ff1cde931c5a27ac22e0df0dd6d3b
ahmed <ahmedelsayed9909@gmail.com>
2024-04-28 23:50:55 +0300

    last update

--- resources/views/admins/Instructions/index.blade.php
+++ resources/views/admins/Instructions/index.blade.php
@@ -23,24 +23,4 @@
         </div>
 
     </div>
-    <div class="page-body">
-
-        <div class="container-fluid">
-            <div class="page-title">
-                <div class="row">
-                    <div class="col-6">
-                        <h3>{{ __('Instructions') }}</h3>
-                    </div>
-                    <div class="col-6">
-                        <ol class="breadcrumb">
-                            <li class="breadcrumb-item"><a href="{{ route('admins.dashboard') }}"> <i
-                                        data-feather="home"></i></a></li>
-                            <li class="breadcrumb-item active">{{ __('Instructions') }}</li>
-                        </ol>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-    </div>
 @endsection
