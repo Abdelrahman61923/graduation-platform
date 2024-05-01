7532e56113ebdb42626da05da3c7888f2a00d767
Abdelrahman61923 <abdoelrhman1@gmail.com>
2024-05-01 17:15:45 +0300

    graduation-platform

--- resources/views/auth/layouts/app.blade.php
+++ resources/views/auth/layouts/app.blade.php
@@ -1,44 +1,75 @@
-
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- CSRF Token -->
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>{{ config('app.name', 'Laravel') }} | @yield('title')</title>
 
-        <!-- Fonts -->
-        <link rel="dns-prefetch" href="//fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{ config('app.name', 'Laravel') }} | @yield('title')</title>
 
-        <!-- Styles -->
-        <link href="{{ asset('css/login.css') }}" rel="stylesheet">
-        <link href="{{ asset('css/all.min.css') }}" rel="stylesheet">
-        <link href="{{ asset('webfonts') }}" rel="stylesheet">
-        <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/vendors/bootstrap.css') }}">
-        <!-- App css-->
-        <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/style.css') }}">
-        <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/vendors/select2.css') }}">
-        @yield('styles')
-    </head>
-    <style>
-        .alert-success {
-            color: #3c763d;
-            background-color: #dff0d8;
-            border-color: #d6e9c6;
-            margin-bottom: 14px;
-            padding: 10px;
-        }
-    </style>
-    <body>
-        @yield('content')
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
-        {{-- @include('sweetalert::alert') --}}
-        {{-- <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script> --}}
-        <script src="{{ asset('assets/js/jquery-3.5.1.min.js') }}"></script>
-        <script src="{{ asset('assets/js/select2/select2.full.min.js') }}"></script>
-        <script src="{{ asset('assets/js/select2/select2-custom.js') }}"></script>
-        <script src="{{ asset('assets/js/Auth/master.js') }}"></script>
-    </body>
+    <!-- Styles -->
+    <link href="{{ asset('css/login.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/all.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('webfonts') }}" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/vendors/bootstrap.css') }}">
+    <!-- App css-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/style.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/vendors/select2.css') }}">
+    @yield('styles')
+</head>
+<style>
+    .alert-success {
+        color: #3c763d;
+        background-color: #dff0d8;
+        border-color: #d6e9c6;
+        margin-bottom: 14px;
+        padding: 10px;
+    }
+
+    body {
+        background-image: url(../images/un.jpg);
+        background-size: cover;
+        background-position: center;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        min-height: 100vh;
+    }
+
+    .circle::before {
+        content: "";
+        position: absolute;
+        background-color: #FFFFFF;
+        width: 170px;
+        height: 150px;
+        border-radius: 50%;
+        top: 140px;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        z-index: -1;
+    }
+
+    form img {
+        width: 180px;
+        margin: -40px 0px 0px 40px;
+        margin-bottom: 10px;
+    }
+</style>
+
+<body>
+    @yield('content')
+
+    {{-- @include('sweetalert::alert') --}}
+    {{-- <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script> --}}
+    <script src="{{ asset('assets/js/jquery-3.5.1.min.js') }}"></script>
+    <script src="{{ asset('assets/js/select2/select2.full.min.js') }}"></script>
+    <script src="{{ asset('assets/js/select2/select2-custom.js') }}"></script>
+    <script src="{{ asset('assets/js/Auth/master.js') }}"></script>
+</body>
+
 </html>
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,50 +3,71 @@
     Login
 @endsection
 @section('content')
-<div class="page">
-    <h1>graduation platform</h1>
-    <form action="{{ route('login') }}" method="POST" style="height: auto;">
-        @csrf
-        <img src="{{ asset('images/loo.png') }}" alt="grade">
-        <div>
-            <i class="fa-solid fa-user icon"></i>
-            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="Email" autofocus>
-            @error('email')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
-        </div>
-        <div>
-            <i class="fa-solid fa-lock icon"></i>
-            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Password">
-            @error('password')
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
-        </div>
-        <div class="row mb-3">
-            <div class="col-md-6 offset-md-4">
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-lg-4">
+                <div class="text-center position-relative">
+                    <h1 class="mt-0 text-light" style="margin-bottom: 90px">Graduation Platform</h1>
+                    <form action="{{ route('login') }}" method="POST" class="circle bg-white"
+                        style="border-radius: 20px;padding-bottom: 20px;">
+                        @csrf
+                        <img src="{{ asset('images/loo.png') }}" alt="grade" class="mb-4">
+                        <div class="container">
+                            <div class="row  px-md-40">
+                                <div class="col-md-12">
+                                    <div class="input-group mb-3">
+                                        <span class="input-group-text"><i class="fa-solid fa-user"></i></span>
+                                        <input id="email" type="email"
+                                            class="form-control @error('email') is-invalid @enderror" name="email"
+                                            value="{{ old('email') }}" autocomplete="email" placeholder="Email" autofocus>
+                                        @error('email')
+                                            <div class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </div>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="col-md-12">
+                                    <div class="input-group mb-3">
+                                        <span class="input-group-text"><i class="fa-solid fa-lock"></i></span>
+                                        <input id="password" type="password"
+                                            class="form-control @error('password') is-invalid @enderror" name="password"
+                                            autocomplete="current-password" placeholder="Password">
+                                        @error('password')
+                                            <div class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </div>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="col-md-12">
+                                    <div class="form-check mb-3">
+                                        <div class="d-flex align-items-center justify-content-between">
+                                            <div>
+                                                <input class="form-check-input" type="checkbox" name="remember"
+                                                    id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                                <label class="form-check-label"
+                                                    for="remember">{{ __('Remember Me') }}</label>
+                                            </div>
+                                            <div>
+                                                @if (Route::has('password.request'))
+                                                    <a href="{{ route('password.request') }}"> Forgot Password?</a>
+                                                @endif
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
 
-                    <label class="form-check-label" for="remember">
-                        {{ __('Remember Me') }}
-                    </label>
+                            </div>
+                        </div>
+                        <button type="submit" class="btn btn-primary w-75 fw-bold fs-6" name="submit">Login</button>
+                        <div class="mt-3 d-flex justify-content-center">
+                            <p>Don't have an account?</p>
+                            <a href="{{ route('register') }}" class="text-decoration-none">Register</a>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
-        <div class="admin">
-            @if (Route::has('password.request'))
-                <a href="{{ route('password.request') }}"> Forgot Password?</a>
-            @endif
-        </div>
-        <button type="submit" name="submit">Login</button>
-        <div class="link" style="margin-top: 10px;">
-            <p>Don't have an account?</p>
-            <a style="margin-top: -15px;font-size: 14px;" href="{{ route('register') }}">Register</a>
-        </div>
-    </form>
-</div>
+    </div>
 @endsection
