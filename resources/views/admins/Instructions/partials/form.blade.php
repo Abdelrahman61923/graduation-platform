145a5ed7e722dfe080ce3e24ac319bd66806fb00
Abdelrahman61923 <abdoelrhman1@gmail.com>
2024-06-20 14:19:46 +0300

    instrictions

--- resources/views/admins/Instructions/partials/form.blade.php
+++ resources/views/admins/Instructions/partials/form.blade.php
@@ -7,16 +7,19 @@
     <div class="col-md-12">
         <div class="mb-3">
             <label for="">{{ __('Instruction') }}<span class="redstar">*</span></label>
-            <textarea id="instruction" class="form-control shadow-none"
-                name="instruction" value="{{ old('instruction') }}" autocomplete="instruction"
-                placeholder="{{ __('Instruction') }}" autofocus>{{ old('instruction', $instruction->instruction) }}</textarea>
-
+            <textarea id="instruction" class="form-control shadow-none @error('instruction') is-invalid @enderror" name="instruction" value="{{ old('instruction') }}"
+                autocomplete="instruction" placeholder="{{ __('Instruction') }}" autofocus>{{ old('instruction', $instruction->instruction) }}</textarea>
+            @error('instruction')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
         </div>
     </div>
     <div class="col-md-12">
         <div class="mb-3">
             <label for="">{{ __('Type') }}<span class="redstar">*</span></label>
-            <select class="form-select shadow-none " name="type" id="type">
+            <select class="form-select shadow-none @error('type') is-invalid @enderror" name="type" id="type">
                 <option selected>{{ __('Select Type') }}</option>
                 @foreach ($types as $type)
                     <option value="{{ $type['id'] }}"
@@ -25,7 +28,11 @@
                     </option>
                 @endforeach
             </select>
-
+            @error('type')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
         </div>
     </div>
 </div>
